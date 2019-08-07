43f8099bd3e11c72e4e14c69b438e78631e14776
陆毅 <javaandlinux@163.com>
2019-08-08 01:19:37 +0800

    i DON'T KNOW

---
+++ bmall-api/src/main/java/com/ty/bmall/service/CatalogService.java
@@ -0,0 +1,13 @@
+package com.ty.bmall.service;
+
+import com.ty.bmall.bean.PmsBaseCatalog1;
+import com.ty.bmall.bean.PmsBaseCatalog2;
+import com.ty.bmall.bean.PmsBaseCatalog3;
+
+import java.util.List;
+
+public interface CatalogService {
+    List<PmsBaseCatalog1> getCatalog1();
+    List<PmsBaseCatalog2> getCatalog2(String catalogId);
+    List<PmsBaseCatalog3> getCatalog3(String catalogId);
+}
