fa927e0de6545796e8d7a8e9d70513d49b1e76cd
陆毅 <javaandlinux@163.com>
2019-08-08 00:31:15 +0800

    完成平台元数据管理以及平台属性修改
    Signed-off-by: 陆毅 <javaandlinux@163.com>

---
+++ bmall-manager-service/src/main/java/com/ty/bmall/mapper/PmsBaseAttrValueMapper.java
@@ -0,0 +1,7 @@
+package com.ty.bmall.mapper;
+
+import com.ty.bmall.bean.PmsBaseAttrValue;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface PmsBaseAttrValueMapper extends Mapper<PmsBaseAttrValue> {
+}
